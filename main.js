20e2622cee20426892bc2637290a4d3d6373f82c
khadijadaniyal <162747729+khadijadaniyal@users.noreply.github.com>
2024-03-14 02:59:24 +0500

    update

---
+++ main.js
@@ -0,0 +1,40 @@
+var leaderName = "Quaid_e_Azam";
+console.log(leaderName);
+var firstLeader = 1;
+console.log(firstLeader);
+var isGreatLeader = true;
+console.log(isGreatLeader);
+var userName = "Ayzal";
+console.log(userName);
+var age = 2;
+console.log(age);
+var isgoodgirl = true;
+console.log(isgoodgirl);
+//casses
+//camelCasses
+var fullName = "daniyal";
+console.log(fullName);
+//snake_case
+var full_name = "daniyal";
+console.log(full_name);
+//PascalCase
+var FullName = "daniyal";
+console.log(FullName);
+//array[]
+var students = ["ansa", "abrish", "abiha"];
+console.log(students);
+var guest = ["shayan", "ayzal", "haya",];
+console.log(guest);
+var guestList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,];
+console.log(guestList);
+var isIftarParty = [true, false, true];
+console.log(isIftarParty);
+//index 0.....
+var medicineBox = ["panadol", "postanforte", "norvasic", 1, 1, 2, 3, 4, 5, true, false,];
+console.log(medicineBox[1], medicineBox[6], medicineBox[8]);
+var medicineName = ["arinac", "merol", "setamet", "nuberolforte",];
+console.log(medicineName[0], medicineName[3]);
+var medicineList = [1, 3, 2, 4, 6, 87, 89, 65,];
+console.log(medicineList[4], medicineList[7]);
+var isExpireMedicine = [true, false];
+console.log(isExpireMedicine[1]);
---
+++ main.ts
@@ -0,0 +1,43 @@
+let leaderName: string = "Quaid_e_Azam"; 
+console.log(leaderName);
+let firstLeader: number = 1;
+console.log(firstLeader);
+let isGreatLeader: boolean = true;
+console.log(isGreatLeader);
+let  userName: string = "Ayzal";
+console.log(userName);
+let age: number = 2;
+console.log(age);
+let isgoodgirl: boolean = true;
+console.log(isgoodgirl); 
+
+  //casses
+  //camelCasses
+  let fullName = "daniyal";
+  console.log(fullName);
+  //snake_case
+  let full_name = "daniyal";
+  console.log(full_name);
+  //PascalCase
+  let FullName = "daniyal";
+  console.log(FullName);
+
+  //array[]
+  let students = ["ansa", "abrish", "abiha"];
+  console.log(students);
+  let guest: string[] = ["shayan" , "ayzal", "haya",];
+  console.log(guest);
+  let guestList: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,];
+  console.log(guestList);
+  let isIftarParty: boolean[] = [true,false,true];
+  console.log(isIftarParty);
+
+//index 0.....
+let medicineBox: any[] = ["panadol", "postanforte", "norvasic", 1,1,2,3,4,5,true ,false,];
+console.log(medicineBox[1], medicineBox[6] ,medicineBox[8]);
+let medicineName: string[] = ["arinac", "merol","setamet","nuberolforte",];
+console.log(medicineName[0], medicineName[3]);
+let medicineList: number[] = [1,3,2,4,6,87,89,65,];
+console.log(medicineList[4], medicineList[7]);
+let isExpireMedicine: boolean[] =  [true,false];
+console.log(isExpireMedicine[1]);
